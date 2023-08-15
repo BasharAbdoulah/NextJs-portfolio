06cefc0a8168e6cd23d3f645507685d0c96fa080
basharabdoulah <des.basharabdulah@gmail.com>
2023-08-15 07:17:31 +0300

    Version 3

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -5,4 +5,7 @@ sitemap({
   baseUrl: "https://www.basharabdullah.com",
   pagesDirectory: path.resolve(__dirname, "../pages"),
   targetDirectory: path.resolve(__dirname, "../pages"),
+  ignoredExtensions: ["js", "map", "css", "jpeg", "png", "xml", "jpg", "icon"],
+  ignoredPaths: ["404", "favicon", "index"],
+  extraPaths: ["/"],
 });
